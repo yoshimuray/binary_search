fc027ef851a7f761e5ae1a10a4c5cd39fcf5adcb
Yoshimura Yusei <yoshimura.y.ag@m.titech.ac.jp>
2018-06-19 16:52:25 +0900

    array.c

--- src/array.c
+++ src/array.c
@@ -5,14 +5,30 @@ int k;
 int A[100000];
 
 
+int binary_search(int B[], int x, int l, int u){
+    int lb = l;
+    int ub = u;
+    while(ub - lb > 1){
+        int m = (ub + lb) / 2;
+        if(B[m] >= x) ub = m;
+        else lb = m;
+    }
+    printf("%d\n",ub);
+    return ub;
+}
+
+
 int main(){
   int i, lb, ub;
   scanf("%d%d", &n, &k);
   for(i = 0; i < n; i++){
     scanf("%d", &A[i]);
   }
-
-
-
+    ub = n;
+    lb = -1;
+    
+    binary_search(A, k, lb, ub);
+    
   return 0;
 }
+
