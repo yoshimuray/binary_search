1a5ab4d5203cb0490bfc41b155cdcf8f068a4acf
Yoshimura Yusei <yoshimura.y.ag@m.titech.ac.jp>
2018-06-22 17:08:44 +0900

    apple.c

--- src/apple.c
+++ src/apple.c
@@ -4,6 +4,23 @@ int n;
 int k;
 int A[100000];
 
+int p(int Ap[], int kp, int np, int mp){
+    
+    int i,j;
+    j = 0;
+    int am = Ap[mp];
+    
+    while(j < kp){
+        for(i = 0; i < np; i++){
+        while(Ap[i] > 0){
+            Ap[i] = Ap[i] - am;
+            j = j + 1;
+            printf("%d\n", j);
+        }}
+        return 1;
+    }
+    return 0;
+}
 
 int main(){
   int i, lb, ub;
@@ -11,7 +28,16 @@ int main(){
   for(i = 0; i < n; i++){
     scanf("%d", &A[i]);
   }
+    
+    lb = -1;
+    ub = n;
+    while(ub - lb > 1){
+        int m = (ub + lb) / 2;
+        if(p(A, k, n, m)) ub = m;
+        else lb = m;
+        printf("c = %d/n", );
+    }
 
-
+    printf("%d\n", A[ub]);
   return 0;
 }
